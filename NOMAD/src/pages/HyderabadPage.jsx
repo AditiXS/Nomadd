79228161699d4a5a6e65e6757fd5561713dd3e5d
AditiXs <sainiaditi220@gmail.com>
2026-05-23 22:36:39 +0530

    feat: add layered collage for Delhi page header

--- NOMAD/src/pages/HyderabadPage.jsx
+++ NOMAD/src/pages/HyderabadPage.jsx
@@ -368,16 +368,31 @@ const HyderabadPage = () => {
 
       {/* HERO */}
       <div className="city-hero" ref={heroRef}>
-        <div className="city-hero-layered-collage">
-          <img src="/hyd/Untitled (1).png" alt="" className="layered-img img-1" />
-          <img src="/hyd/Untitled.png" alt="" className="layered-img img-2" />
-          <img src="/hyd/image (3).png" alt="" className="layered-img img-4" />
-          <img src="/hyd/img.png" alt="" className="layered-img img-5" />
-          <img src="/hyd/biryani.png" alt="" className="layered-img img-9" />
-          <img src="/hyd/imgg.png" alt="" className="layered-img img-6" />
-          <img src="/hyd/human.png" alt="" className="layered-img img-7" />
-          <img src="/dancing.png" alt="" className="layered-img img-8" />
-        </div>
+        {cityName.toLowerCase() === 'delhi' ? (
+          <div className="city-hero-layered-collage delhi-hero-collage">
+            {/* The 9 Delhi images from public/delhi */}
+            <img src="/delhi/c9dfdfe838386eeb00ec6b43ff613950.png" alt="" className="layered-img delhi-img-1" />
+            <img src="/delhi/fc8ec2c32db8368c9aacc9810a9ca25f.png" alt="" className="layered-img delhi-img-2" />
+            <img src="/delhi/0666861e91a09d14019dcbfccc0f235b.png" alt="" className="layered-img delhi-img-3" />
+            <img src="/delhi/01859cdef37cdfec19e80d714b7b9c26.png" alt="" className="layered-img delhi-img-4" />
+            <img src="/delhi/27806531de5894111d2ae850a2ec5ddf.png" alt="" className="layered-img delhi-img-5" />
+            <img src="/delhi/95258dcab710aa96d1be4a3ffbf7de15.png" alt="" className="layered-img delhi-img-6" />
+            <img src="/delhi/Untitled - May 20, 2026 at 21.52.57.png" alt="" className="layered-img delhi-img-7" />
+            <img src="/delhi/4cd09432d553c88a6f4175be5ea86ca7.png" alt="" className="layered-img delhi-img-8" />
+            <img src="/delhi/Untitled - May 23, 2026 at 22.16.45.png" alt="" className="layered-img delhi-img-9" />
+          </div>
+        ) : (
+          <div className="city-hero-layered-collage">
+            <img src="/hyd/Untitled (1).png" alt="" className="layered-img img-1" />
+            <img src="/hyd/Untitled.png" alt="" className="layered-img img-2" />
+            <img src="/hyd/image (3).png" alt="" className="layered-img img-4" />
+            <img src="/hyd/img.png" alt="" className="layered-img img-5" />
+            <img src="/hyd/biryani.png" alt="" className="layered-img img-9" />
+            <img src="/hyd/imgg.png" alt="" className="layered-img img-6" />
+            <img src="/hyd/human.png" alt="" className="layered-img img-7" />
+            <img src="/dancing.png" alt="" className="layered-img img-8" />
+          </div>
+        )}
         <div className="city-hero-fade" />
         <button className="city-back-btn" onClick={() => navigate('/login')}>←</button>
       </div>
